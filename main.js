97c87cb7663f666eb0b7eb6f4aee00362689a319
Sanjana-git123 <67508020+Sanjana-git123@users.noreply.github.com>
2020-07-24 10:12:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -21,20 +21,20 @@ function getResults (query) {
   
   function displayResults (weather) {
     let city = document.querySelector('.location .city');
-    city.innertext = `${weather.name}, ${weather.sys.country}`;
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
   
     let now = new Date();
-  let temp = document.querySelector('.location .temp');
-  date.innertext = dateBuilder(now);
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
 
-  let date = document.querySelector('.current .date');
+  let temp = document.querySelector('.current .temp');
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
 
   let weather_el = document.querySelector('.current .weather');
-  weather_el.innertext = weather.weather[0].main;
+  weather_el.innerText = weather.weather[0].main;
 
   let hilow = document.querySelector('.hi-low');
-  hilow.innertext = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
 }
 
 function dateBuilder (d) {
