05b630d794b344ad1393d02262988192cd053b00
Sanjana-git123 <sanjana18199@gmail.com>
2020-07-25 20:31:03 +0530

    updated js

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 const api = {
-    key: "ce64f4d1aa6cf83a25daa5a28adcc85e",
-    base: "https://api.openweathermap.org/data/2.5/"
-    }
+  key: "ce64f4d1aa6cf83a25daa5a28adcc85e",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
-    const searchbox = document.querySelector('.search-box');
-    searchbox.addEventListener('keypress', setQuery);
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
-  if (evt.keycode == 13) {
-  getResults(searchbox.value);
-}
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
 }
 
 function getResults (query) {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
     .then(weather => {
       return weather.json();
     }).then(displayResults);
-  }
-  
-  function displayResults (weather) {
-    let city = document.querySelector('.location .city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
-  
-    let now = new Date();
+}
+
+function displayResults (weather) {
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
   let date = document.querySelector('.location .date');
   date.innerText = dateBuilder(now);
 
@@ -38,13 +38,13 @@ function getResults (query) {
 }
 
 function dateBuilder (d) {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-  
-    return `${day} ${date} ${month} ${year}`;
-  }
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
